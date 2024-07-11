4c795d2582e9a875c4ec50c3c7bafad70b1cfc1e
Runa Brandes <runabrandes@gmail.com>
2024-07-11 21:12:11 +0000

    Installed Django AllAuth

--- mountain_mist/urls.py
+++ mountain_mist/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("spa_home.urls"), name='spa_home_urls'),
+    path("accounts/", include("allauth.urls")),
 ]
