fd542a14966436fe592a6fa10f37e5e047ce51f6
Runa Brandes <runabrandes@gmail.com>
2024-07-11 19:57:36 +0000

    Merged service_list.html into index.html
    Linked to base.html

--- spa_home/urls.py
+++ spa_home/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.ServiceList.as_view(), name='home'),
+    path('', views.SalonIntroduction.as_view(), name='home'),
 ]
--- spa_home/views.py
+++ spa_home/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic import TemplateView
 from .models import Service
 
 # Create your views here.
-
-class ServiceList(generic.ListView):
-    model = Service
+class SalonIntroduction(TemplateView, generic.ListView):
+     template_name = 'spa_home/index.html'
+     object_list = Service.objects.all()
