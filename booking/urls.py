532dc64b24cabd100a43fdfdd0b8f35877884576
Runa Brandes <runabrandes@gmail.com>
2024-07-27 21:52:53 +0000

    Added url for past_bookings

--- booking/urls.py
+++ booking/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.booking_request, name='booking'),
     path('booking_overview', views.BookingListView.as_view(), name='booking_overview'),
-    path('cancel/<int:booking_id>', views.cancel_booking, name='cancel_booking')
+    path('past_bookings', views.PastBookingListView.as_view(), name='past_bookings'),
+
 ]
