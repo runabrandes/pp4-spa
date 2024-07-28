45c18e8ea8a77f0bab34c5d9181fdf02c02d7ef4
Runa Brandes <runabrandes@gmail.com>
2024-07-28 11:13:35 +0000

    Added urls for cancelling and editing bookings

--- booking/urls.py
+++ booking/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.booking_request, name='booking'),
     path('booking_overview', views.BookingListView.as_view(), name='booking_overview'),
+    path('cancel/<int:booking_id>', views.cancel_booking, name='cancel_booking'),
     path('past_bookings', views.PastBookingListView.as_view(), name='past_bookings'),
+    path('edit/<int:booking_id>', views.edit_booking, name='edit_booking'),
+
 
 ]
